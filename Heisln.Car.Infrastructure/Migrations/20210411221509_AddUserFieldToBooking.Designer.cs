925065baa1ee2bdf9ce3e427fd7537623c7d268e
Martin Leeb <martin.leeb@cssteam.at>
2021-04-12 00:19:33 +0200

    add infrastructure

--- Heisln.Car.Infrastructure/Migrations/20210411221509_AddUserFieldToBooking.Designer.cs
+++ Heisln.Car.Infrastructure/Migrations/20210411221509_AddUserFieldToBooking.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Heisln.Car.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Heisln.Car.Infrastructure.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210411221509_AddUserFieldToBooking")]
+    partial class AddUserFieldToBooking
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
